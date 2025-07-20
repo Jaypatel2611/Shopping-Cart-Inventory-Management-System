ac9007d4024fac4754a2541107573d18929d8661
Jay <jp261118@gmail.com>
2025-07-20 14:07:03 +0530

    Refactored

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,47 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import Modules.Auth.Auth;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        Auth auth = new Auth();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while (true) {
+            System.out.println("\n🔐 --------- Authentication Menu ---------");
+            System.out.println("1. 📝 Sign Up");
+            System.out.println("2. 🔓 Login");
+            System.out.println("3. 🚪 Exit");
+            System.out.print("👉 Enter your choice: ");
+
+            int choice = sc.nextInt();
+
+            try {
+                switch (choice) {
+                    case 1:
+                        System.out.println("📝 Sign Up selected.");
+                        auth.signUp();
+                        break;
+                    case 2:
+                        System.out.println("🔓 Login selected.");
+                        auth.userLogin();
+                        /* TODO : add logic for multiple user login and role management
+                           User user = auth.getUser();
+                        if (user != null) {
+                            CustomerManagement.start(auth.getUser());
+                        }*/
+                        break;
+                    case 3:
+                        System.out.println("👋 Exiting... Thank you for visiting!");
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("❌ Invalid choice! Please try again.");
+                }
+            } catch (SQLException e) {
+                System.out.println("❌ Database error: " + e.getMessage());
+            }
         }
     }
 }
