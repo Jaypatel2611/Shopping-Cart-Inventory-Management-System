267dc406b5d3c1025102480704b75c3782c251f4
dlegend10 <dakshtrivedi646@gmail.com>
2025-08-18 10:43:20 +0530

    Add files via upload

--- src/Modules/Auth/Auth.java
+++ src/Modules/Auth/Auth.java
@@ -1,25 +1,22 @@
 package Modules.Auth;
 
+import Database.Database;
+import Modules.Address.Address;
+import Modules.Users.AdminManagement.AdminManagement;
+import Modules.Users.CustomerManagement.CustomerManagement;
 import Modules.Users.User;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Scanner;
 
+
+
+//import static Modules.Users.CustomerManagement.CustomerManagement.user;
+
 public class Auth {
-    static Connection con;
-
-    static {
-        try {
-            con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3307/myjdbc_2025?allowPublicKeyRetrieval=true&useSSL=false",
-                    "jay_mysql",
-                    "pass"
-            );
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     Scanner sc = new Scanner(System.in);
 
     private boolean isValidEmail(String email) {
@@ -38,12 +35,48 @@ public class Auth {
         return password.matches(pattern);
     }
 
-    public void signUp() throws SQLException {
-        System.out.print("Enter First Name : ");
-        String firstName = sc.next();
+    // Method to check if first name is only alphabets
+    public static boolean isValidFirstName(String name) {
+        return name.matches("[a-zA-Z]+");
+        }
+    public static boolean isValidLastName(String name) {
+        return name.matches("[a-zA-Z]+");
+    }
+
+
+    public void signUp() throws Exception {
+        Connection con = Database.getCon();
+
+        String firstName;
+        do {
+            System.out.print("Enter First Name : ");
+            firstName = sc.next().toLowerCase();
+            if (isValidFirstName(firstName)) {
+                System.out.println("Valid first name ✅");
+                break;
+            } else {
+                System.out.println("Invalid first name ❌ (only letters allowed)");
+            }
+
+        }
+        while (true);
         System.out.print("Enter Last Name : ");
-        String lastName = sc.next();
+        String lastName;
+        // String lastName ;
+        do {
+            System.out.print("Enter last Name : ");
+            lastName = sc.next().toLowerCase();
+            if (isValidLastName(lastName)) {
+                System.out.println("Valid Last name ✅");
+                break;
+            } else {
+                System.out.println("Invalid last name ❌ (only letters allowed)");
+            }
+
+        }
+        while (true);
         String email;
+        sc.nextLine();
         do {
             System.out.print("Enter your email: ");
             email = sc.nextLine().trim();
@@ -62,6 +95,7 @@ public class Auth {
             mobileNo = sc.nextLine().trim();
 
             if (isValidMobileNo(mobileNo)) {
+                System.out.println("✅ Valid mobile No!");
                 break;
             } else {
                 System.out.println("❌ Invalid mobile number! It must contain exactly 10 digits.");
@@ -82,42 +116,104 @@ public class Auth {
                 System.out.println("   → At least one digit");
                 System.out.println("   → At least one special character (!@#$%^&* etc.)");
             }
+//            System.out.println("Enter Modules.Address In formatted way ");
+//            System.out.println("Enter Modules.Address Line 1 : ");
+//            String addressLine1 = sc.nextLine();
+//            sc.nextLine();
+//            System.out.println("Enter Modules.Address Line 2 : ");
+//            String addressLine2 = sc.nextLine();
+//            System.out.println("Enter Area : ");
+//            String area = sc.nextLine();
+//            System.out.println("Enter City : ");
+//            String city = sc.nextLine();
+//            System.out.println("Enter State : ");
+//            String state = sc.nextLine();
+//            System.out.println("Enter Pin code : ");
+//            int pinCode = sc.nextInt();
+//
+//            User user = User.getCurrentUser();
+//            Address add = new Address(user.getFirstName(), addressLine1, addressLine2, area, city, state, pinCode, user);
+
+
+        }
+//        System.out.println("select your role ");
+//        String role = sc.next().toLowerCase();
+//        if (role.equalsIgnoreCase("admin")) {
+//           // System.out.println("Enter Admin Password");
+//            //String Adminpassword ;
+//            String Adminpassword = "Admin@123";
+//            while (true) {
+//                System.out.print("Enter Admin Password : ");
+//                password = sc.next();
+//
+//                if (isValidPassword(password) && password.equalsIgnoreCase("Admin@123")) {
+//                    AdminManagement am = new AdminManagement();
+//                    break;
+//                } else {
+//                    System.out.println("❌ Password must be at least 8 characters long, contain:");
+//                    System.out.println("   → At least one uppercase letter");
+//                    System.out.println("   → At least one digit");
+//                    System.out.println("   → At least one special character (!@#$%^&* etc.)");
+//                }
+//            }
+
+            String insertUser = "INSERT INTO users(first_name,last_name,username,mobile_no,email,password,role) VALUES(?,?,?,?,?,?,?)";
+            try (PreparedStatement insertStmt = con.prepareStatement(insertUser)) {
+
+                insertStmt.setString(1, firstName);
+                insertStmt.setString(2, lastName);
+                insertStmt.setString(3, userName);
+                insertStmt.setString(4, mobileNo);
+                insertStmt.setString(5, email);
+                insertStmt.setString(6, password);
+                insertStmt.setString(7, "user");
+                int rows = insertStmt.executeUpdate();
+                if (rows > 0) {
+                    ResultSet keys = insertStmt.getGeneratedKeys();
+                    if (keys.next()) {
+                        int newUser = keys.getInt(1);
+                    }
+                    PreparedStatement ps = Database.getCon().prepareStatement("select * from users where user_id = ?");
+                    ResultSet rss = ps.executeQuery();
+                    if (rss.next()) {
+                        User.addLoggedInUser(rss);
+                    }
+                }
+
+            } catch (Exception e) {
+                //throw new RuntimeException(e);
+            }
+            System.out.println("✅ Signed Up Successfully");
         }
 
-        String insertUser = "INSERT INTO user(firstName,lastName,userName,password,role) VALUES(?,?,?,?,?)";
-        try (PreparedStatement insertStmt = con.prepareStatement(insertUser)) {
-            insertStmt.setString(1, firstName);
-            insertStmt.setString(2, lastName);
-            insertStmt.setString(3, userName);
-            insertStmt.setString(4, password);
-            insertStmt.executeUpdate();
-        }
 
-        System.out.println("✅ Signed Up Successfully");
-    }
 
-    public void userLogin() throws SQLException {
+    public int userLogin() throws Exception {
+    Scanner sc = new Scanner(System.in);
+        Connection con = Database.getCon();
         System.out.print("Enter userName : ");
         String userName = sc.next();
         System.out.print("Enter Password : ");
         String password = sc.next();
 
-        String fetchUserDetails = "SELECT * FROM user WHERE userName = ? AND password = ?";
+        String fetchUserDetails = "SELECT * FROM users WHERE username = ? AND password = ?";
         try (PreparedStatement insertStmt = con.prepareStatement(fetchUserDetails)) {
             insertStmt.setString(1, userName);
             insertStmt.setString(2, password);
 
             ResultSet rs = insertStmt.executeQuery();
-            if (rs.next()) {
+            while (rs.next()) {
                 String fetchedPassword = rs.getString("password");
                 if (password.equals(fetchedPassword)) {
                     System.out.println("✅ Logged In Successfully");
-                    // TODO : add a heterogeneous primitive data type to add user
-                    User.addLoggedInUser();
+                    User.addLoggedInUser(rs);
+                    CustomerManagement.start();
+                    return rs.getInt("user_id");
                 } else {
                     System.out.println("❌ Invalid Credentials");
                 }
             }
         }
+        return 0;
     }
 }
