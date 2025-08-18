9a8164afb4013d667609c8f2a279d6ac5687719c
dlegend10 <dakshtrivedi646@gmail.com>
2025-08-18 10:42:00 +0530

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,29 @@
+import Database.Database;
 import Modules.Auth.Auth;
+import Modules.Users.AdminManagement.AdminManagement;
+import Modules.Users.CustomerManagement.CustomerManagement;
+import Modules.Users.User;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        String billTable = "create table if not exists bills(bill_id int auto_increment primary key,customer_id int references users(user_id),bill_date date references orders(order_date),bill longblob)";
+        PreparedStatement ps = Database.getCon().prepareStatement(billTable);
+        ps.executeUpdate();
         Scanner sc = new Scanner(System.in);
         Auth auth = new Auth();
-
-        while (true) {
+        int choice;
+        do{
             System.out.println("\n🔐 --------- Authentication Menu ---------");
             System.out.println("1. 📝 Sign Up");
             System.out.println("2. 🔓 Login");
             System.out.println("3. 🚪 Exit");
             System.out.print("👉 Enter your choice: ");
 
-            int choice = sc.nextInt();
+            choice = sc.nextInt();
 
             try {
                 switch (choice) {
@@ -25,7 +33,22 @@ public class Main {
                         break;
                     case 2:
                         System.out.println("🔓 Login selected.");
-                        auth.userLogin();
+                        int userId = auth.userLogin();
+
+                        if (User.getCurrentUser().getUserId() != 0) {
+                            if (User.getCurrentUser().getRole().equalsIgnoreCase("admin")) {
+                                try {
+                                    AdminManagement.main(args);
+                                } catch (Exception e) {
+                                    throw new RuntimeException(e);
+                                }
+                            } else {
+                               // CustomerManagement.start(User.getUserById(userId));
+                            }
+                        } else {
+                            System.out.println("❌ Login failed. Invalid credentials.");
+                        }
+
                         /* TODO : add logic for multiple user login and role management
                            User user = auth.getUser();
                         if (user != null) {
@@ -34,14 +57,17 @@ public class Main {
                         break;
                     case 3:
                         System.out.println("👋 Exiting... Thank you for visiting!");
+                        //CustomerManagement customerManagement = new CustomerManagement();
+
                         System.exit(0);
                         break;
                     default:
                         System.out.println("❌ Invalid choice! Please try again.");
                 }
-            } catch (SQLException e) {
+            } catch (Exception e) {
                 System.out.println("❌ Database error: " + e.getMessage());
             }
         }
+        while (choice!=3);
     }
 }
